18262c311fa2c1307c0f500248e2c2e8b1969d7f
AKFR0ST <alex.frosters@gmail.com>
2026-05-05 12:59:40 +0300

    Fix Student controller.

--- src/main/java/com/sb2/controller/StudentController.java
+++ src/main/java/com/sb2/controller/StudentController.java
@@ -3,21 +3,26 @@ package com.sb2.controller;
 import com.sb2.dto.student.StudentRequest;
 import com.sb2.dto.student.StudentResponse;
 import com.sb2.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/student")
+@Tag(name = "Student", description = "API для редактирования карточек учеников")
 @RequiredArgsConstructor
 public class StudentController {
 
     private final StudentService service;
 
+    @Operation(summary = "Создать новую карточку")
     @PostMapping
     public StudentResponse create(@RequestBody StudentRequest request) {
         return service.create(request);
     }
 
+    @Operation(summary = "Получить карточку по id ученика")
     @GetMapping("/{id}")
     public StudentResponse get(@PathVariable Long id) {
         return service.get(id);
